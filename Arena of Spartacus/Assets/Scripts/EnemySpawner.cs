11a53d51fef27294d3cb85443bdcee336dcc5920
nikkimartin9555 <96130076+nikkimartin9555@users.noreply.github.com>
2023-03-28 13:21:36 -0400

    Copied Dylan's working Unity project folder

---
+++ Arena of Spartacus/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    [SerializeField]
+    private float enemyInterval = 3.5f;
+
+    public EnemyAi enmeyAi;
+
+    float x;
+    float y;
+    float z;
+
+    private void Start()
+    {
+        StartCoroutine(spawnEnemy(enemyInterval, enemyPrefab));
+        
+        
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        x = Random.Range(-35, 35);
+        y = .5f;
+        z = Random.Range(-45, 50);
+
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(x, y, z), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+        enmeyAi.agent.Warp(transform.position);
+        enmeyAi.agent.enabled = true;
+        enmeyAi.ChasePlayer();
+        
+    }
+}
