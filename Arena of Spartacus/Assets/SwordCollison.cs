4d83617baf7f4874130b358c1c88e0b688f5ca58
DylanNagy <DNAGY.1.2122@tech4maine.org>
2023-03-30 11:20:38 -0400

    dd

--- Arena of Spartacus/Assets/SwordCollison.cs
+++ Arena of Spartacus/Assets/SwordCollison.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.XR.Interaction.Toolkit;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
 
 public class SwordCollison : MonoBehaviour
 {
     // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+       if(Input.GetButtonDown("XRI_Right_Trigger"))
+        {
+            Debug.Log("HI");
+        }
     }
 }
