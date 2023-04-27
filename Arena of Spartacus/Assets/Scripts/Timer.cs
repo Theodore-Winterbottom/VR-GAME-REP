c4d9811bb58db88d3cb0b0f677b0cbd50e39494e
nikkimartin9555 <96130076+nikkimartin9555@users.noreply.github.com>
2023-04-27 13:32:46 -0400

    time

--- Arena of Spartacus/Assets/Scripts/Timer.cs
+++ Arena of Spartacus/Assets/Scripts/Timer.cs
@@ -2,39 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class Timer : MonoBehaviour
 {
 
     public TextMeshProUGUI timerText;
 
-    public float currentTime = 1;
-    public int countUp;
+    public float timer;
+    public int countUp = 1;
 
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
         // Repeats IEnumerator Time method
-        StartCoroutine(Time());
+        //StartCoroutine(Time());
+    }
+
+    void ToStartCoroutine(int countUp)
+    {
+        // When Botton GameObject is pressed Timer is active
+        if (gameObject.tag == ("Botton"))
+        {
+            // Repeats IEnumerator Time method
+            StartCoroutine(Time());
+        }
     }
 
     IEnumerator Time()
     {
         // Anything under will start every second
         yield return new WaitForSeconds(1);
-        
-        if (currentTime == 0)
-        {
-            currentTime += countUp++;
-        }
-        
+
+        AddTime(countUp);
 
 
         // Anything above will start every second and repeat
         StartCoroutine(Time());
     }
-
     
+    void AddTime(int countUp)
+    {
+        //if ()
+        {
+            
+        }
+    }
 
 }
