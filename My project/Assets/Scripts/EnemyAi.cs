d697200f65b13f34f839b9820ddef870294212c8
Theodore Winterbottom <100698585+Theodore-Winterbottom@users.noreply.github.com>
2023-05-02 13:38:08 -0400

    ikkkkkkkkkkkkkkkkkkkkk

--- My project/Assets/Scripts/EnemyAi.cs
+++ My project/Assets/Scripts/EnemyAi.cs
@@ -25,6 +25,56 @@ public class EnemyAi : MonoBehaviour
 
     [SerializeField] private float health;
 
+
+    [SerializeField] private Rigidbody rb;
+
+    public float healthAmount = 100;
+
+    //Point towards the instantiated Object will move
+    Transform goal;
+
+    //Reference to the NavMeshAgent
+    UnityEngine.AI.NavMeshAgent agent1;
+
+    // Use this for initialization
+    void Start()
+    {
+        //You get a reference to the destination point inside your scene
+        goal = GameObject.Find("Player").GetComponent<Transform>();
+
+        //Here you get a reference to the NavMeshAgent
+        agent1 = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        //You indicate to the agent to what position it has to move
+        agent1.destination = goal.position;
+    }
+
+    public void TakeDamage(float Damage)
+    {
+        healthAmount -= Damage;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Sword")
+        {
+            TakeDamage(50);
+        }
+    }
+
+    void Update()
+    {
+
+        agent1.destination = goal.position;
+
+        if (healthAmount <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
     private void FixedUpdate()
     {
         //Check for sight and attack range
