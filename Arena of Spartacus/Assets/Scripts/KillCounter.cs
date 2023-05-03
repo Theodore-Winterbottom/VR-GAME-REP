a79133ebf433f621c018e9cc6f01f86f55c3cec0
nikkimartin9555 <96130076+nikkimartin9555@users.noreply.github.com>
2023-05-03 13:35:05 -0400

    kill counter

--- Arena of Spartacus/Assets/Scripts/KillCounter.cs
+++ Arena of Spartacus/Assets/Scripts/KillCounter.cs
@@ -2,24 +2,110 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class KillCounter : MonoBehaviour
 {
-
+    // Kill counter text
     public TextMeshProUGUI killCounterText;
+    // Kill counter variable
+    public int killCount = 0;
 
-    public int kills;
+    public int damage = 10;
 
-    public GameObject enemy;
+    private void Start()
+    {
+        // 
+        killCounterText = GetComponent<TextMeshProUGUI>();
+    }
 
-    public float health;
+    private void Update()
+    {
+        // 
+        killCounterText.text = killCount.ToString();
+    }
+    // Method to add kills
+    void AddKills()
+    {
+        // Adds kills
+        killCount++;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        EnemyAi enemy = other.gameObject.GetComponent<EnemyAi>();
+        
+        if (enemy != null)
+        {
+            //enemy.TakeDamage(damage);
+        }
+    }
 
 
 
-    // When the player is hit by an enemy player takes damage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*/ When the player is hit by an enemy player takes damage
     private void OnCollisionEnter(Collision collision)
     {
         // Player deals damage to enemy if plyer hits enemy
@@ -33,52 +119,6 @@ public class KillCounter : MonoBehaviour
     }
 
    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     /*public TextMeshProUGUI killCounterText;
